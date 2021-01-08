e1db8bb9e4d9050ed8df2921c9c5b398e64f8ad5
Daniel <mabadejedaniel1@gmail.com>
2021-01-07 20:29:58 -0800

    added file

---
+++ myfirstwordpressplugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Tuts+ Register Post Types
+Plugin URI: https://localhost
+Description: Plugin to accompany tutsplus guide to creating plugins, registers a post type.
+Version: 1.0
+Author: Daniel Mabadeje
+Author URI: https://danielmabadeje.netlify.app/
+License: GPLv2 or later
+Text Domain: firstplugin
+*/
