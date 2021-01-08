43ff13c32050078193165fe63773ca66502803e3
Daniel <mabadejedaniel1@gmail.com>
2021-01-07 20:32:22 -0800

    adding function

--- myfirstwordpressplugin.php
+++ myfirstwordpressplugin.php
@@ -46,3 +46,39 @@ function tutsplus_register_post_type() {
     register_post_type( 'tutsplus_movie', $args );
          
 }
+
+
+add_action( 'init', 'tutsplus_register_post_type' );
+
+
+
+function tutsplus_register_taxonomy() {    
+     
+    // books
+    $labels = array(
+        'name' => __( 'Genres' , 'tutsplus' ),
+        'singular_name' => __( 'Genre', 'tutsplus' ),
+        'search_items' => __( 'Search Genres' , 'tutsplus' ),
+        'all_items' => __( 'All Genres' , 'tutsplus' ),
+        'edit_item' => __( 'Edit Genre' , 'tutsplus' ),
+        'update_item' => __( 'Update Genres' , 'tutsplus' ),
+        'add_new_item' => __( 'Add New Genre' , 'tutsplus' ),
+        'new_item_name' => __( 'New Genre Name' , 'tutsplus' ),
+        'menu_name' => __( 'Genres' , 'tutsplus' ),
+    );
+     
+    $args = array(
+        'labels' => $labels,
+        'hierarchical' => true,
+        'sort' => true,
+        'args' => array( 'orderby' => 'term_order' ),
+        'rewrite' => array( 'slug' => 'genres' ),
+        'show_admin_column' => true,
+        'show_in_rest' => true
+ 
+    );
+     
+    register_taxonomy( 'tutsplus_genre', array( 'tutsplus_movie' ), $args);
+     
+}
+add_action( 'init', 'tutsplus_register_taxonomy' );
